892d96fe4d47747f4bb7c452603c75677173e25b
huang22 <1048507371@qq.com>
2023-10-08 08:14:32 +0000

    init

---
+++ unicode_blocks/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+Author  huang22
+Date    ：2023/10/8 15:36
+"""
+from setuptools import setup,find_packages
+if __name__ == '__main__':
+    setup(
+        name="unicodeblocks",
+        version="1.0",
+        author="huang22",
+        description="unicode blocks",
+        packages=find_packages()
+    )
